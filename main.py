66325decaba33d5eb609bf34bc5034395d6ca864
axelmoreno0942 <axel.moreno@epitech.eu>
2025-10-06 13:44:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import sys
 
 pg.init()
 screen = pg.display.set_mode((480, 640))
+playing = True
+
+while playing:
+    for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            playing = False
+
+pg.quit()
